99dfe002270e54690b0b7e22312531983ed39e04
MF <mirco.fuhr@student.uni-siegen.de>
2016-04-05 18:37:25 +0200

    Changes3

--- js/bewerbung.js
+++ js/bewerbung.js
@@ -35,3 +35,14 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$(document).ready(function(){
+	
+	$('.skillbar').skillBars({
+		from: 0,
+		speed: 4000, 
+		interval: 100,
+		decimals: 0,
+	});
+	
+});
