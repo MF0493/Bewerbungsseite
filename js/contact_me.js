462917f4e19c23ccb3386bbf9b9413879afa7018
MF <mirco.fuhr@student.uni-siegen.de>
2016-04-05 23:49:47 +0200

    Changes4

--- js/contact_me.js
+++ js/contact_me.js
@@ -37,7 +37,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Ihre Nachricht wurde erfolgreich versendet. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -49,7 +49,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Entschuldigung " + firstName + ", es scheint so als ob der Mailserver nicht erreichbar ist. Bitte versuchen sie es Später noch einmmal.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
